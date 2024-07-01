7cd975a840b5c4f42754d2baebc90ff5387e93be
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-07-02 03:45:25 +0500

    crypto-news

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function showAlert() {
+    alert('Bitcoinni narxi : 788,627,332.70UZS');
+}
+function showAlert2() {
+    alert('Ethereum narxi : 43,086,214.50UZS');
+}
+function showAlert3() {
+    alert('Solana narxi : 1,542,000UZS');
+}
+function showAlert4() {
+    alert('PEPE narxi : 0,14UZS');
+}
+function showAlert5() {
+    alert('SHIB narxi : 0,22UZS');
+}
+function showAlert6() {
+    alert('BNB narxi : 587,96UZS');
+}
